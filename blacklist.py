0d13284a1f2254f31d672adf2b5d60fef41800cc
Adnan Ardhian <ardhianadnan@gmail.com>
2018-03-30 19:30:34 +0700

    Create blacklist.py

---
+++ blacklist.py
@@ -0,0 +1,17 @@
+def initialize(blacklist):
+    file_object = open("D:/# Adnan/Reimbursement/New folder/"+blacklist, "r")
+    input_file = file_object.read()
+    input_file = input_file.split('\n')
+    input_file = input_file[1:len(input_file)-2]
+    input_file = list(map(str, input_file))
+    return input_file
+
+def check_blacklist(name, phone_number):
+    string_check = name+" "+str(phone_number)
+    if(string_check) in blacklist_file:
+        return True
+    else:
+        return False
+
+blacklist_file = initialize("blacklist_file.txt")
+check_blacklist("Andi",1341441)
